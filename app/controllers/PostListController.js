9ff177a02a4d0ec988836515e96be438221484c0
Braden <braden.herndon@gmail.com>
2015-06-27 13:23:09 -0500

    Page buttons working, still no list update

--- app/controllers/PostListController.js
+++ app/controllers/PostListController.js
@@ -7,12 +7,19 @@
 controllers
     
      .controller('PostListController', ['$scope', '$http', function($scope, $http) {
-        $http.get('data/posts.json').success(function(data) {
-            $scope.posts = data;
-        });
 
         $scope.currentPage = 0;
         $scope.pageSize = 5;
+        $scope.posts = [];
+
+        $scope.numberOfPages = function() {
+            return Math.ceil($scope.posts.length/$scope.pageSize);
+        }
+
+        $http.get('data/posts.json').success(function(data) {
+            $scope.posts = data.reverse();
+
+        });
 
 
     }]);
