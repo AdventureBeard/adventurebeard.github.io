03f06809677376ca76f116c36196ec6d0372643c
Braden <braden.herndon@gmail.com>
2015-07-31 00:18:07 -0500

    Add functionality to load posts from json

--- app/components/posteditor/PostEditorController.js
+++ app/components/posteditor/PostEditorController.js
@@ -1,61 +1,66 @@
 angular.module('app.controllers')
     .controller('PostEditorController', ['$scope', '$rootScope', '$timeout', '$http', function ($scope, $rootScope, $timeout, $http) {
 
-        var today = new Date();
-        var mm = today.getMonth() + 1;
-        var dd = today.getDay();
-        var yyyy = today.getFullYear();
+        $scope.editorposts = undefined;
+        var editor = undefined;
 
-        $scope.date = mm + "/" + dd + "/" + yyyy;
-
-        var opts = {
-            container: 'epiceditor',
-            textarea: null,
-            basePath: 'bower_components/EpicEditor/epiceditor',
-            clientSideStorage: true,
-            localStorageName: 'epiceditor',
-            useNativeFullscreen: true,
-            parser: marked,
-            file: {
-                name: 'epiceditor',
-                defaultContent: '',
-                autoSave: 100
-            },
-            theme: {
-                base: '/themes/base/epiceditor.css',
-                preview: '/themes/preview/github.css',
-                editor: '/themes/editor/epic-light.css'
-            },
-            button: {
-                preview: true,
-                fullscreen: true,
-                bar: "show"
-            },
-            focusOnLoad: false,
-            shortcut: {
-                modifier: 18,
-                fullscreen: 70,
-                preview: 80
-            },
-            string: {
-                togglePreview: 'Toggle Preview Mode',
-                toggleEdit: 'Toggle Edit Mode',
-                toggleFullscreen: 'Enter Fullscreen'
-            },
-            autogrow: false
+        function loadEditor(startingContent) {
+            var opts = {
+                container: 'epiceditor',
+                textarea: null,
+                basePath: 'bower_components/EpicEditor/epiceditor',
+                clientSideStorage: true,
+                localStorageName: 'epiceditor',
+                useNativeFullscreen: true,
+                parser: marked,
+                file: {
+                    name: 'epiceditor',
+                    defaultContent: 'TEST TEST TEST',
+                    autoSave: 100
+                },
+                theme: {
+                    base: '/themes/base/epiceditor.css',
+                    preview: '/themes/preview/github.css',
+                    editor: '/themes/editor/epic-light.css'
+                },
+                button: {
+                    preview: true,
+                    fullscreen: true,
+                    bar: "show"
+                },
+                focusOnLoad: false,
+                shortcut: {
+                    modifier: 18,
+                    fullscreen: 70,
+                    preview: 80
+                },
+                string: {
+                    togglePreview: 'Toggle Preview Mode',
+                    toggleEdit: 'Toggle Edit Mode',
+                    toggleFullscreen: 'Enter Fullscreen'
+                },
+                autogrow: false
+            }
+            editor = new EpicEditor(opts).load();
+            editor.reflow('Height')
         }
-        var editor = new EpicEditor(opts).load();
-        editor.reflow('Height')
+
+        $scope.loadPost = function(id) {
+            console.log("Got id: " + id);
+            $scope.selectedPost = $scope.editorposts[id-1];
+            editor.importFile(null, $scope.selectedPost.content);
+            editor.load();
+
+        }
 
 
-        $scope.items = [
-            {id: 1, title: 'Scooby Doo'},
-            {id: 2, title: 'Shaggy Rodgers'},
-            {id: 3, title: 'Fred Jones'},
-            {id: 4, title: 'Daphne Blake'},
-            {id: 5, title: 'Velma Dinkley'},
-        ];
 
+        $http.get('data/posts.json').success(function (data) {
+            $scope.editorposts = data.reverse();
+            $scope.selectedPost = $scope.editorposts[0];
+            loadEditor($scope.selectedPost.content);
+
+        });
 
         $rootScope.showNavbar = false;
 
