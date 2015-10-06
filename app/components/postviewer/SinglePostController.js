66e0131cca641f5b83eca4dd3bc594db2577efac
Braden Herndon <braden.herndon@gmail.com>
2015-10-06 17:17:14 -0500

    Add post title to site title

--- app/components/postviewer/SinglePostController.js
+++ app/components/postviewer/SinglePostController.js
@@ -5,14 +5,12 @@
 
 angular.module('app.controllers')
     
- .controller('SinglePostController', ['$scope', '$rootScope', '$routeParams', '$location', 'DataService', function($scope, $rootScope, $routeParams, $location, DataService) {
+ .controller('SinglePostController', ['$scope', '$rootScope', '$routeParams', '$location','$window', 'DataService', function($scope, $rootScope, $routeParams, $location, $window, DataService) {
 
         $scope.content = null;
         $scope.id = $routeParams;
         $scope.disqusId = $routeParams.id;
         $scope.disqusUrl = $location.absUrl();
-        console.log("ID:" + $scope.disqusId);
-        console.log("URL:" + $scope.disqusUrl);
         $scope.contentLoaded = false;
 
 
@@ -21,6 +19,7 @@ angular.module('app.controllers')
                 $location.url('#!/');
             }
             $scope.title = callback[0].title;
+            $window.document.title = "AdventureBeard | " + $scope.title;
             $scope.content = markdown.toHTML(callback[0].content);
             $scope.date = callback[0].date;
             $scope.contentLoaded = true;
